2cc7687ed29e269d0144972a4e9cfdcce275a27d
Hannes Ponstingl <hp3@sanger.ac.uk>
2021-03-03 11:59:21 +0000

    Changed java memory requirements for parent nextflow job.

--- example_runtime/scripts/start_nextflow_lsf.sh
+++ example_runtime/scripts/start_nextflow_lsf.sh
@@ -6,7 +6,7 @@
 # conda activate nextflow
 
 # run nextflow main.nf with inputs and lsf config:
-export NXF_OPTS="-Xms5G -Xmx5G"
+export NXF_OPTS="-Xms2G -Xmx2G"
 nextflow run ./nextflow_ci/pipelines/main.nf \
       -c ./nextflow_ci/nextflow.config -c inputs.nf -profile lsf \
       --nf_ci_loc $PWD -resume
